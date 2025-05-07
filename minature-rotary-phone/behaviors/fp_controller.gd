b614a6b574530dc1cbd8da847538a72836c8ddf0
Eric O'Brien <ericobrien246@gmail.com>
2025-05-07 20:55:35 +0100

    Player movement and bird model
    Made Bird model with CSG shapes and ready to add scene so will wander in main scene. Player movement to get around the main scene

--- minature-rotary-phone/behaviors/fp_controller.gd
+++ minature-rotary-phone/behaviors/fp_controller.gd
@@ -1,17 +1,19 @@
-extends Node3D
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+extends CharacterBody3D
 
 @export var sensitivity = 0.1
 @export var speed:float = 1.0
 
 var controlling = true
 
-var left:XRController3D
-var right:XRController3D
+@export var left:XRController3D
+@export var right:XRController3D
+
+# Snap turn movement for right controller XR
+@export var snap_angle: float = 30.0
+@export var snap_deadzone: float = 0.5
+@export var snap_cooldown: float = 0.3
+
+var snap_timer = 0.0
 
 
 func _input(event):
@@ -49,16 +51,37 @@ func _process(delta):
 			var cam_basis = $XROrigin3D/XRCamera3D.global_transform.basis
 			global_translate(cam_basis.z * speed * mult * delta * -joy.y)
 			global_translate(cam_basis.x * speed * mult * delta * joy.x)
+			
+		if right:
+			snap_turing(delta)
 				
 		var turn = Input.get_axis("turn_left", "turn_right") - v.x	
 		if abs(turn) > 0:     
 			global_translate(global_transform.basis.x * speed * turn * mult * delta)
 		
 		var movef = Input.get_axis("move_forward", "move_back")
-		print(movef)
+		#print(movef)
 		if abs(movef) > 0:     
 			global_translate(global_transform.basis.z * speed * movef * mult * delta)
 		
 		var upanddown = Input.get_axis("move_up", "move_down")
 		if abs(upanddown) > 0:     
 			global_translate(- global_transform.basis.y * speed * upanddown * mult * delta)
+		
+		move_and_slide()	
+			
+func snap_turing(delta):
+	
+	if snap_timer > 0:
+		snap_timer -= delta
+		return
+		
+	var joy = right.get_vector2("primary")
+	
+	if abs(joy.x) > snap_deadzone:
+		var turn_direction = sign(joy.x)
+		var turn_angle = snap_angle * -turn_direction
+		
+		rotate(Vector3.UP, deg_to_rad(turn_angle))
+		
+		snap_timer = snap_cooldown
